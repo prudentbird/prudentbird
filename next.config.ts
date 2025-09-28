f2882c98755f060b5723b5f99fe71a7516facbcb
prudentbird <prudentbird@gmail.com>
2025-09-28 01:51:53 +0100

    chore: add configuration files and update dependencies for new project structure

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    ppr: true,
+    cacheComponents: true,
+    browserDebugInfoInTerminal: true,
+  },
 };
 
 export default nextConfig;
