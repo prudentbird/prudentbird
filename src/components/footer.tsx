324da738f380ff660dfb6cf58b5fa89879855ffe
prudentbird <prudentbird@gmail.com>
2026-04-17 03:33:51 +0100

    chore: update footer year

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <footer className="py-6 border-t flex justify-between items-center">
       <div className="text-sm text-muted-foreground">
         <p>BN 8524538</p>
-        <p>© 2025 PrudentBird</p>
+        <p>© {new Intl.DateTimeFormat('en-UK', { year: 'numeric' }).format()} PrudentBird</p>
       </div>
       <ThemeSwitcher />
     </footer>
